4e8505e2f47b3e80a5001e17255c4ad085a99ea4
riofauzi708 <riofauzi708@gmail.com>
2024-01-07 00:08:45 +0700

    initial commit

--- asset/js/blog.js
+++ asset/js/blog.js
@@ -3,17 +3,17 @@ const projects = []
    function addProject(event) {
         event.preventDefault()
 
-        const projectName = document.getElementById("projectName").value
-        const startDate = document.getElementById("startDate").value
-        const endDate = document.getElementById("endDate").value
-        const description = document.getElementById("description").value
-        const nodeJs = document.getElementById("nodeJs").value
-        const nextJs = document.getElementById("nextJs").value
-        const reactJs = document.getElementById("reactJs").value
-        const typeScript = document.getElementById("typeScript").value
-        const uploadImage = document.getElementById("uploadImage").files
+        const projectName = document.getElementById("projectName").value;
+        const startDate = document.getElementById("startDate").value;
+        const endDate = document.getElementById("endDate").value;
+        const description = document.getElementById("description").value;
+        const nodeJs = document.getElementById("nodeJs").value;
+        const nextJs = document.getElementById("nextJs").value;
+        const reactJs = document.getElementById("reactJs").value;
+        const typeScript = document.getElementById("typeScript").value;
+        const uploadImage = document.getElementById("uploadImage").files;
 
-        // const uploadImageLink = URL.createObjectURL(uploadImage[0])
+        const uploadImageLink = URL.createObjectURL(uploadImage[0])
 
         // localStorage.setItem("project", JSON.stringify, ({
         //     projectName,
@@ -32,7 +32,7 @@ const projects = []
             nextJs,
             reactJs,
             typeScript,
-            uploadImage
+            uploadImage : uploadImageLink
         }
 
         projects.push(project)
@@ -41,26 +41,41 @@ const projects = []
         console.log("projects", projects)
 
 
-        console.log("projectName", projectName)
-        console.log("startDate", startDate)
-        console.log("endDate", endDate)
-        console.log("description", description)
-        console.log("nodeJs", nodeJs)
-        console.log("nextJs", nextJs)
-        console.log("reactJs", reactJs)
-        console.log("typeScript", typeScript)
-        console.log("uploadImage", uploadImage[0])
+        
     }
 
 
     function renderProject() {
-        let html = ""
+        let html = ``
 
         for(let index= 0; index < projects.length; index++) {
-            html += ``
+            html += `
+        <div class="project-layout">
+            <div class="blog-form-bg">
+            <img class="img-blog-style" src="${projects[index].uploadImage}" alt="#">
+            <h3><a href="#">${projects[index].projectName}</a></h3>
+            <p class="dates">durasi : 3 bulan</p>
+            <p class="container">${projects[index].description}</p>
+
+            <div class="icon-layout">
+                <img class="logo-icons" src="asset/img/logo-playstore.png" alt="">
+                <img class="logo-icons" src="asset/img/logo-android.png" alt="">
+                <img class="logo-icons" src="asset/img/logo-java.png" alt="">
+                <img class="logo-icons" src="asset/img/logo-javascript.png" alt="">
+            </div>
+                
+            <div class="btn-setting">
+                <button class="btn-edit-dlt">edit</button>
+                <button class="btn-edit-dlt">delete</button>
+
+            </div>
+
+        </div>`
         }
 
-        document.getElementById("projects").innerHTML = "html"
+        document.getElementById("contents").innerHTML = html
+        
     }
+  
 
     renderProject()
