09aaeab6acf5ef71e781006b14d6e04385db5a1c
riofauzi708 <riofauzi708@gmail.com>
2024-01-07 20:55:12 +0700

    initial commit

--- asset/js/blog.js
+++ asset/js/blog.js
@@ -3,15 +3,15 @@ const projects = []
    function addProject(event) {
         event.preventDefault()
 
-        const projectName = document.getElementById("projectName").value;
-        const startDate = document.getElementById("startDate").value;
-        const endDate = document.getElementById("endDate").value;
-        const description = document.getElementById("description").value;
-        const nodeJs = document.getElementById("nodeJs").value;
-        const nextJs = document.getElementById("nextJs").value;
-        const reactJs = document.getElementById("reactJs").value;
-        const typeScript = document.getElementById("typeScript").value;
-        const uploadImage = document.getElementById("uploadImage").files;
+        const projectName = document.getElementById("projectName").value
+        const startDate = document.getElementById("startDate").value
+        const endDate = document.getElementById("endDate").value
+        const description = document.getElementById("description").value
+        const nodeJs = document.getElementById("nodeJs").value
+        const nextJs = document.getElementById("nextJs").value
+        const reactJs = document.getElementById("reactJs").value
+        const typeScript = document.getElementById("typeScript").value
+        const uploadImage = document.getElementById("uploadImage").files
 
         const uploadImageLink = URL.createObjectURL(uploadImage[0])
 
@@ -35,13 +35,21 @@ const projects = []
             uploadImage : uploadImageLink
         }
 
-        projects.push(project)
+        projects.unshift(project)
 
         renderProject()
         console.log("projects", projects)
 
 
-        
+        console.log("projectName", projectName)
+        console.log("startDate", startDate)
+        console.log("endDate", endDate)
+        console.log("description", description)
+        console.log("nodeJs", nodeJs)
+        console.log("nextJs", nextJs)
+        console.log("reactJs", reactJs)
+        console.log("typeScript", typeScript)
+        console.log("uploadImage", uploadImage[0])
     }
 
 
@@ -50,12 +58,13 @@ const projects = []
 
         for(let index= 0; index < projects.length; index++) {
             html += `
-        <div class="project-layout">
             <div class="blog-form-bg">
-            <img class="img-blog-style" src="${projects[index].uploadImage}" alt="#">
-            <h3><a href="#">${projects[index].projectName}</a></h3>
+            <img class="img-blog-style" src="asset/img/painting-mountain.jpg" alt="">
+            <h3><a class="text-deco" href=""> Nama Project</a></h3>
             <p class="dates">durasi : 3 bulan</p>
-            <p class="container">${projects[index].description}</p>
+            <p class="container">Drawing is a means where everything is realized by illustrating in two-dimensional form
+                 as an outpouring or thought in various forms such as slides, portraits, paintings, films, 
+                 blurry projectors, or strips (Hamalik, 2017: 43).</p>
 
             <div class="icon-layout">
                 <img class="logo-icons" src="asset/img/logo-playstore.png" alt="">
@@ -70,12 +79,11 @@ const projects = []
 
             </div>
 
-        </div>`
+        </div>
+    `
         }
 
         document.getElementById("contents").innerHTML = html
-        
     }
-  
 
     renderProject()
