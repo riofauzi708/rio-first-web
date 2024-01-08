c553add268715dfc6142a265947780f00c356111
riofauzi708 <riofauzi708@gmail.com>
2024-01-08 15:29:19 +0700

    initial commit

--- asset/js/blog.js
+++ asset/js/blog.js
@@ -1,134 +1,95 @@
-const projects = []
+const projects = [];
 
-   function addProject(event) {
-        event.preventDefault()
+function addProject(event) {
+  event.preventDefault();
 
-        const projectName = document.getElementById("projectName").value
-        const startDate = document.getElementById("startDate").value
-        const endDate = document.getElementById("endDate").value
-        const description = document.getElementById("description").value
-        const nodeJs = document.getElementById("nodeJs").checked
-        const nextJs = document.getElementById("nextJs").checked
-        const reactJs = document.getElementById("reactJs").checked
-        const typeScript = document.getElementById("typeScript").checked
-        let uploadImage = document.getElementById("uploadImage").files
+  const projectName = document.getElementById("projectName").value;
+  const startDate = document.getElementById("startDate").value;
+  const endDate = document.getElementById("endDate").value;
+  const description = document.getElementById("description").value;
+  const nodeJs = document.getElementById("nodeJs").checked;
+  const nextJs = document.getElementById("nextJs").checked;
+  const reactJs = document.getElementById("reactJs").checked;
+  const typeScript = document.getElementById("typeScript").checked;
+  let uploadImage = document.getElementById("uploadImage").files;
 
-        uploadImage = URL.createObjectURL(uploadImage[0])
+  uploadImage = URL.createObjectURL(uploadImage[0]);
 
-        // localStorage.setItem("project", JSON.stringify, ({
-        //     projectName,
-        //     startDate,
-        //     endDate,
-        //     description,
-        //     uploadImageLink,
-        // }))
-        
-        const startDateParts = startDate.split("-");
-        const endDateParts = endDate.split("-");
-        const startDateObj = new Date([startDate[2],startDateParts[1],endDateParts[0]].join("-"));
-        const endDateObj = new Date(endDate);
-        const duration = Math.ceil((endDateObj - startDateObj) / (1000 * 60 * 60 * 24));
+  const startDateObj = new Date(startDate);
+  const endDateObj = new Date(endDate);
+  const diffInMonths = (endDateObj.getFullYear() - startDateObj.getFullYear()) * 12 + (endDateObj.getMonth() - startDateObj.getMonth());
 
-        
+  const project = {
+    projectName,
+    startDate: formatTanggal(startDate),
+    endDate: formatTanggal(endDate),
+    startDateNumber: startDateObj.getDate(),
+    endDateNumber: endDateObj.getDate(),
+    duration: diffInMonths,
+    description,
+    nodeJs,
+    nextJs,
+    reactJs,
+    typeScript,
+    uploadImage,
+  };
 
-        const project = {
-            projectName,
-            startDate,
-            endDate,
-            duration,
-            description,
-            nodeJs,
-            nextJs,
-            reactJs,
-            typeScript,
-            uploadImage,
-        }
+  projects.unshift(project);
 
-        projects.unshift(project)
+  renderProject();
+  console.log("projects", projects);
+}
 
-        renderProject()
-        console.log("projects", projects)
+function formatTanggal(tanggal) {
+  const date = new Date(tanggal);
+  const monthNames = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"
+  ];
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+  return `${date.getDate()} ${month} ${year}`;
+}
 
+function renderProject() {
+  let html = "";
 
-        console.log("projectName", projectName)
-        console.log("startDate", startDate)
-        console.log("endDate", endDate)
-        console.log("description", description)
-        console.log("nodeJs", nodeJs)
-        console.log("nextJs", nextJs)
-        console.log("reactJs", reactJs)
-        console.log("typeScript", typeScript)
-        console.log("uploadImage", uploadImage[0])
-        console.log("startDateObj", startDateObj)
-        console.log("endDateObj", endDateObj)
-        console.log("duration", duration)
-    }
+  for (let index = 0; index < projects.length; index++) {
+    html += `
+      <div class="blog-form-bg">
+        <a href="blog-detail.html?startDate=${projects[index].startDate}&endDate=${projects[index].endDate}&duration=${projects[index].duration}">
+          <img class="img-blog-style" src="${projects[index].uploadImage}" alt="">
+        </a>
+        <h3><a class="text-deco" href="blog-detail.html?startDate=${projects[index].startDate}&endDate=${projects[index].endDate}&duration=${projects[index].duration}">${projects[index].projectName}</a></h3>
+        <p class="dates">durasi : ${projects[index].duration} bulan</p>
+        <p class="container">${projects[index].description}</p>
 
-
-    function renderProject() {
-        let html = ``
-
-        for(let index= 0; index < projects.length; index++) {
-            html += `
-            <div class="blog-form-bg">
-            <a href="blog-detail.html">
-            <img class="img-blog-style" src="${projects[index].uploadImage}" alt=""></a>
-            <h3><a class="text-deco" href="blog-detail.html">${projects[index].projectName}</a></h3>
-            <p class="dates">durasi : ${projects[index].duration}</p>
-            <p class="container">${projects[index].description}</p>
-
-            <div class="icon-layout">
-                ${projects[index].nodeJs ? `<img class="logo-icons" src="asset/img/logo-playstore.png" alt="">` : ''}
-                ${projects[index].nextJs ? `<img class="logo-icons" src="asset/img/logo-android.png" alt="">` : ''}
-                ${projects[index].reactJs ? `<img class="logo-icons" src="asset/img/logo-java.png" alt="">` : ''}
-                ${projects[index].typeScript ? `<img class="logo-icons" src="asset/img/logo-javascript.png" alt="">` : ''}
-            </div>
-                
-            <div class="btn-setting">
-                <button class="btn-edit-dlt">edit</button>
-                <button class="btn-edit-dlt">delete</button>
-
-            </div>
-
+        <div class="icon-layout">
+          ${projects[index].nodeJs ? `<img class="logo-icons" src="asset/img/logo-playstore.png" alt="">` : ''}
+          ${projects[index].nextJs ? `<img class="logo-icons" src="asset/img/logo-android.png" alt="">` : ''}
+          ${projects[index].reactJs ? `<img class="logo-icons" src="asset/img/logo-java.png" alt="">` : ''}
+          ${projects[index].typeScript ? `<img class="logo-icons" src="asset/img/logo-javascript.png" alt="">` : ''}
         </div>
-    `
-        }
+                
+        <div class="btn-setting">
+          <button class="btn-edit-dlt">edit</button>
+          <button class="btn-edit-dlt">delete</button>
+        </div>
+      </div>
+    `;
+  }
 
-        document.getElementById("contents").innerHTML = html
-    }
+  document.getElementById("contents").innerHTML = html;
+}
 
-    function getFullTime(dates) {
-        let minutes = dates.getMinutes()
-        let hours = dates.getHours()
-        const date = dates.getDate()
-        const month = dates.getMonth();
-        const year = getFullYear();
-
-
-        if (hours < 10) {
-            hours = "0" + hours
-        }
-
-        if (minutes < 10) {
-            minutes = "0" + minutes
-        }
-
-        const months = ["January", "February", "March",
-        "April", "May", "June", "July", "August",
-        "September", "October", "November", "December"]
-
-        return `${date} ${months[month]} ${year} ${hours}:${minutes} WIB`;
-        
-    }
-
-
-    function dates() {
-        let startDate = new Date(startDate);
-        let endDate = new Date(endDate);
-        
-        
-        let diffInMonths = (endDate.getFullYear() - startDate.getFullYear()) * 12 + (endDate.getMonth() - startDate.getMonth());
-        console.log('durasi :', diffInMonths, 'bulan');
-    }
-
-    renderProject()
+renderProject();
